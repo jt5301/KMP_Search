8ac8fffe9e62193f4a1813d418337b9a9ea18e92
John Tieu <johnson.tieu@gmail.com>
2020-01-05 23:59:47 -0500

    leetcode balnacedbracket

--- leetCodeQs.js
+++ leetCodeQs.js
@@ -202,3 +202,29 @@ var maxSubArray = function(nums) {
 	}
 	return maxSoFar
 };
+
+
+//balanced brackets solution
+
+var isValid = function(s) {
+  let openingBrackets = '({['
+  let bracketLookup = {
+      '[':']',
+      '{':'}',
+      '(':')'
+  }
+  let stack = []
+  for(let i = 0; i< s.length;i++){
+      if(openingBrackets.includes(s[i])){
+          stack.push(s[i])
+      }
+      else{
+          if(s[i]===bracketLookup[stack[stack.length-1]]){
+              stack.pop()
+          }
+          else return false
+      }
+  }
+      if(stack.length===0)return true
+      else{return false}
+};
