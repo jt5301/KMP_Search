25704f5002e6eb592e9cc73bcc9118712d5e61f9
John Tieu <johnson.tieu@gmail.com>
2020-05-06 16:38:37 -0400

    twilio q rewritten, leetcode array qs

--- leetCodeQs.js
+++ leetCodeQs.js
@@ -566,16 +566,65 @@ var checkValidString = function (s) {
 // missing from t, in the order they occur within s
 
 function missingWords(string1, string2) {
-  let str1Arr = string1.split(' ')
-  let str2Arr = string2.split(' ')
-  let str2Obj = {}
-  let missingWords = []
+  let str1Arr = string1.split(' ') //full sentence
+  let str2Arr = string2.split(' ') //subsequence
+  let allMissingWords = []
+  let counter = 0
+  str1A.forEach((current) => {
+    if (current === str2Arr[counter]) counter++
+    else allMissingWords.push(current)
+  })
+  return allMissingWords
 
-  str2Arr.forEach((current) => {
-    str2Obj[current] = true
-  })
-  str1Arr.forEach((current) => {
-    if (!str2Obj[current]) missingWords.push(current)
-  })
-  return missingWords
 }
+
+
+
+// Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+// Each letter in the magazine string can only be used once in your ransom note.
+
+// Note:
+// You may assume that both strings contain only lowercase letters.
+
+// canConstruct("a", "b") -> false
+// canConstruct("aa", "ab") -> false
+// canConstruct("aa", "aab") -> true
+
+var canConstruct = function (ransomNote, magazine) {
+  let magHash = {}
+  for (let i in magazine) {
+    if (!magHash[magazine[i]]) magHash[magazine[i]] = 1
+    else magHash[magazine[i]] += 1
+  }
+  for (let i in ransomNote) {
+    if (magHash[ransomNote[i]] === 0 || !magHash[ransomNote[i]]) return false
+    magHash[ransomNote[i]] -= 1
+  }
+  return true
+
+};
+
+
+// Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+// You may assume that the array is non-empty and the majority element always exist in the array.
+
+// Example 1:
+
+// Input: [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: [2,2,1,1,1,2,2]
+// Output: 2
+
+var majorityElement = function (nums) {
+  let numSize = Math.ceil(nums.length / 2)
+  let numsHash = {}
+  for (let i in nums) {
+    if (!numsHash[nums[i]]) numsHash[nums[i]] = 1
+    else numsHash[nums[i]] += 1
+    if (numsHash[nums[i]] >= numSize) return nums[i]
+  }
+};
