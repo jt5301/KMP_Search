6d5a5db8d5efd2b5e8d184c3cc8eabb158bfb2f7
John Tieu <johnson.tieu@gmail.com>
2020-05-19 01:51:50 -0400

    regex training

--- leetCodeQs.js
+++ leetCodeQs.js
@@ -655,3 +655,20 @@ var invertTree = function (root) {
   return root
 
 };
+
+
+//openasset regex q
+
+const files = ['\\server1\\images\\Healthcare\\8342-St Botolphs Corner\\photographs\\foo.tif', '\\server1\\images\\Commercial\\9453-Range Grove\\rendered\\bar.jpg']
+
+let regex = /\\server1\\images\\(\w+)\\([\w-\s]+)\\(\w+)\\(\w+)/
+// let match = regex.exec(file)
+//   console.log(match)
+for (let i = 0; i < files.length; i++) {
+  let file = files[i]
+  let match = regex.exec(file)
+  console.log(match[1])
+  console.log(match[2])
+  console.log(match[3])
+  console.log(match[4])
+}
