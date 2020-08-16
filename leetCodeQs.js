2455ba75b1c9912057a3d99eef4fabd69c7eb7ef
John Tieu <johnson.tieu@gmail.com>
2020-08-16 01:32:51 -0400

    singlecycle refactor

--- leetCodeQs.js
+++ leetCodeQs.js
@@ -683,30 +683,21 @@ function hasSingleCycle(array) {
   for (let i = 0; i < array.length; i++) {
     indexHash[i] = false
   }
-  debugger
   let curIndex = 0
   while (true) {
     let jump = 0
-    if (array[curIndex] > array.length) {
-      jump = array[curIndex] % array.length
-      curIndex += jump
+    if (array[curIndex] > array.length || array[curIndex] < 0) {
+      jump = Math.abs(array[curIndex]) % array.length
+      array[curIndex] < 0 ? curIndex -= jump : curIndex += jump
     }
-    else {
-      if (array[curIndex] < 0) {
-        jump = Math.abs(array[curIndex]) % array.length
-        curIndex -= jump
-      }
-    }
-    if (jump === 0) curIndex += array[curIndex]
+    else curIndex += array[curIndex]
     if (curIndex > array.length - 1) curIndex = curIndex - array.length
     if (curIndex < 0) curIndex = array.length + (curIndex % array.length)
     if (indexHash[curIndex] === true) break
     indexHash[curIndex] = true
   }
   for (let i in indexHash) {
-    if (indexHash[i] === false) {
-      return false
-    }
+    if (indexHash[i] === false) return false
   }
   return true
 }
