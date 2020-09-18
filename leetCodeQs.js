6a3a7613f92940231b16b5d1129d0f4cc9f96f78
John Tieu <johnson.tieu@gmail.com>
2020-09-18 01:29:21 -0400

    possiblesums

--- leetCodeQs.js
+++ leetCodeQs.js
@@ -796,3 +796,59 @@ function containsCloseNums(nums, k) {
   }
   return false
 }
+
+/*You have a collection of coins, and you know the values of the coins and the quantity of each type of coin in it. You want to know how many distinct sums you can make from non-empty groupings of these coins.
+
+Example
+
+For coins = [10, 50, 100] and quantity = [1, 2, 1], the output should be
+possibleSums(coins, quantity) = 9.
+
+Here are all the possible sums:
+
+50 = 50;
+10 + 50 = 60;
+50 + 100 = 150;
+10 + 50 + 100 = 160;
+50 + 50 = 100;
+10 + 50 + 50 = 110;
+50 + 50 + 100 = 200;
+10 + 50 + 50 + 100 = 210;
+10 = 10;
+100 = 100;
+10 + 100 = 110.
+As you can see, there are 9 distinct sums that can be created from non-empty groupings of your coins. */
+
+function possibleSums(coins, quantity) {
+  let coinsArray = []
+  let coinCounter = 0
+  let quantCounter = 0
+  while (coinCounter < coins.length) {
+    if (quantCounter < quantity[coinCounter]) {
+      quantCounter += 1
+      coinsArray.push(coins[coinCounter])
+      continue
+    }
+    coinCounter += 1
+    quantCounter = 0
+  }
+  let hashSet = new Set()
+  let currentCoinInd = 0
+  while (currentCoinInd < coinsArray.length) {
+    if (hashSet.size == 0) {
+      hashSet.add(coins[currentCoinInd])
+    }
+    else {
+      let toAdd = []
+      hashSet.forEach((current) => {
+        toAdd.push(coinsArray[currentCoinInd] + current)
+      })
+      toAdd.forEach((current) => {
+        hashSet.add(current)
+      })
+      if (!hashSet.has(coinsArray[currentCoinInd])) hashSet.add(coinsArray[currentCoinInd])
+    }
+    currentCoinInd += 1
+  }
+  return hashSet.size
+}
