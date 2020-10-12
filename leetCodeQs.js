8ab180024842e15534ad2c13112343cd876cca59
John Tieu <johnson.tieu@gmail.com>
2020-10-12 14:06:51 -0400

    fib w memo

--- leetCodeQs.js
+++ leetCodeQs.js
@@ -947,4 +947,16 @@ const minPathSum = (grid) => {
   return solutions[solutions.length - 1][solutions[0].length - 1]
 }
 
-
+//fibonacci w memoization
+var fib = function (n) {
+  const memo = {}
+  function fibHelper(n) {
+    if (memo[n]) return memo[n]
+    if (n <= 1) memo[n] = n
+    else memo[n] = (fibHelper(n - 1) + fibHelper(n - 2))
+    return memo[n]
+  }
+  fibHelper(n)
+  if (n <= 1) return n
+  return memo[n - 2] + memo[n - 1]
+};
