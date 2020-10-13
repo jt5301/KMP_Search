61b02be64b6a54bf8cde6c385a4278e84e0e5241
John Tieu <johnson.tieu@gmail.com>
2020-10-13 14:52:38 -0400

    min number of jumps

--- leetCodeQs.js
+++ leetCodeQs.js
@@ -1030,3 +1030,23 @@ var canJump = function (nums) {
   if (validIndices[0] === 'Good') return true
   else return false
 };
+
+
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+TLE on leetcode, works on algoexpert
+*/
+var jump = function (nums) {
+  let successArray = new Array(nums.length).fill(Infinity)
+  successArray[0] = 0
+  for (let i = 1; i < successArray.length; i++) {
+    for (let k = i - 1; k >= 0; k--) {
+      if (k + nums[k] >= i) successArray[i] = Math.min(successArray[i], successArray[k] + 1)
+    }
+  }
+  return successArray[successArray.length - 1]
+};
