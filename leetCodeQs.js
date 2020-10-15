1d56d4b620ce3728c79ca7f1a7ffcda474bed2f5
John Tieu <johnson.tieu@gmail.com>
2020-10-15 01:54:22 -0400

    asteroids

--- leetCodeQs.js
+++ leetCodeQs.js
@@ -1173,3 +1173,42 @@ UndergroundSystem.prototype.getAverageTime = function (startStation, endStation)
     return accum += current
   }, 0) / stationTimes.length
 };
+
+// We are given an array asteroids of integers representing asteroids in a row.
+
+// For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+// Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+//stack Q
+var asteroidCollision = function (asteroids) {
+  debugger
+  let returnAsteroids = [asteroids[0]]
+  for (let i = 1; i < asteroids.length; i++) {
+    if (returnAsteroids[returnAsteroids.length - 1] > 0 && asteroids[i] < 0) {
+      while (returnAsteroids.length >= 0) {
+        if (returnAsteroids[returnAsteroids.length - 1] < 0) {
+          returnAsteroids.push(asteroids[i])
+          break
+        }
+        if (asteroids[i] * -1 === returnAsteroids[returnAsteroids.length - 1]) {
+          returnAsteroids.pop()
+          break
+        }
+        if (asteroids[i] * -1 > returnAsteroids[returnAsteroids.length - 1]) {
+          returnAsteroids.pop()
+        }
+        else {
+          if (returnAsteroids.length === 0) {
+            returnAsteroids.push(asteroids[i])
+            break
+          }
+          break
+        }
+      }
+    }
+    else {
+      returnAsteroids.push(asteroids[i])
+    }
+  }
+  return returnAsteroids
+};
