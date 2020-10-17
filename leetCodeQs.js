08744a0587308d29d883452b391215bce21d6264
John Tieu <johnson.tieu@gmail.com>
2020-10-17 01:40:21 -0400

    merge interval

--- leetCodeQs.js
+++ leetCodeQs.js
@@ -1378,3 +1378,28 @@ function maxSumIncreasingSubsequence(array) {
   }, 0)
   return [returnSum, path]
 }
+
+// Given a collection of intervals, merge all overlapping intervals.
+
+// Example 1:
+
+// Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+var merge = function (intervals) {
+  if (intervals.length === 0) return []
+  intervals.sort((a, b) => {
+    return a[0] - b[0]
+  })
+  const mergedIntervals = [intervals[0]]
+  for (let i = 1; i < intervals.length; i++) {
+    let currentInterval = intervals[i]
+    let latestIntervalInReturn = mergedIntervals[mergedIntervals.length - 1]
+    if (currentInterval[0] >= latestIntervalInReturn[0] && currentInterval[0] <= latestIntervalInReturn[1]) {
+      mergedIntervals[mergedIntervals.length - 1][1] = Math.max(currentInterval[1], mergedIntervals[mergedIntervals.length - 1][1])
+    }
+    else mergedIntervals.push(currentInterval)
+  }
+  return mergedIntervals
+};
