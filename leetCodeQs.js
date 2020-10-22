347f78f21af7e3446d3f5de247c830f667b5365c
John Tieu <johnson.tieu@gmail.com>
2020-10-22 13:55:49 -0400

    remove all adjacent dupes in string 1

--- leetCodeQs.js
+++ leetCodeQs.js
@@ -1649,6 +1649,17 @@ function findExtra(strA, strB) {
   return false
 }
 
+// Remove All Adjacent Duplicates in String I
+var removeDuplicates = function (s) {
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      s = s.slice(0, i) + s.slice(i + 2)
+      i = -1
+    }
+  }
+  return s
+};
+
 // Remove All Adjacent Duplicates in String II
 var removeDuplicates = function (s, k) {
   let hashCheck = {}
