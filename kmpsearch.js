bb3ec5ac75fca66ee35431167e6c9eac50f33e4a
John Tieu <johnson.tieu@gmail.com>
2020-11-04 17:50:13 -0500

    number of good pairs

--- kmpsearch.js
+++ kmpsearch.js
@@ -67,3 +67,36 @@ var minCostClimbingStairs = function (cost) {
   // then you return the lowest of the last two numbers in the array, since you can take either one or two steps
   return Math.min(dp[dp.length - 1], dp[dp.length - 2])
 };
+
+// 1512. Number of Good Pairs
+// Given an array of integers nums.
+
+// A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+// Return the number of good pairs.
+
+
+
+// Example 1:
+
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+
+var numIdenticalPairs = function (nums) {
+  let goodPairs = 0
+  let hash = {}
+  for (let i = 0; i < nums.length; i++) {
+    if (!hash[nums[i]]) {
+      hash[nums[i]] = { frequency: 1, combo: 0 }
+    } else {
+      hash[nums[i]].combo += hash[nums[i]].frequency
+      hash[nums[i]].frequency += 1
+    }
+  }
+  for (let num in hash) {
+    goodPairs += hash[num].combo
+  }
+  return goodPairs
+};
