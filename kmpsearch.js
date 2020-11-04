6c33044dbe7cad3875345a1daef442aaa42cbeba
John Tieu <johnson.tieu@gmail.com>
2020-11-04 00:14:02 -0500

    min cost climibing stairs

--- kmpsearch.js
+++ kmpsearch.js
@@ -1,40 +1,40 @@
-function kpmSearch(string, pattern){
+function kpmSearch(string, pattern) {
   let lpsArray = [0]
   let prefixInd = 0
   let suffixInd = 1
-  while(suffixInd < pattern.length){
-    if(pattern[prefixInd]===pattern[suffixInd]){
-      lpsArray.push(prefixInd+1)
-      prefixInd+=1
-      suffixInd+=1
+  while (suffixInd < pattern.length) {
+    if (pattern[prefixInd] === pattern[suffixInd]) {
+      lpsArray.push(prefixInd + 1)
+      prefixInd += 1
+      suffixInd += 1
     }
-    else{
-      if(prefixInd===0){
+    else {
+      if (prefixInd === 0) {
         lpsArray.push(prefixInd)
-        suffixInd+=1
+        suffixInd += 1
       }
-      else{
-        prefixInd = lpsArray[prefixInd-1]
+      else {
+        prefixInd = lpsArray[prefixInd - 1]
       }
     }
   }
   console.log(lpsArray)
   let stringInd = 0
   let patternInd = 0
-  while(stringInd<string.length){
-    if(string[stringInd]===pattern[patternInd]){
-      if(patternInd === pattern.length-1){
-        return stringInd-pattern.length+1
+  while (stringInd < string.length) {
+    if (string[stringInd] === pattern[patternInd]) {
+      if (patternInd === pattern.length - 1) {
+        return stringInd - pattern.length + 1
+      }
+      stringInd += 1
+      patternInd += 1
     }
-    stringInd+=1
-    patternInd+=1
-  }
-    else{
+    else {
       if (patternInd > 0) {
-      patternInd = lpsArray[patternInd - 1];
-    }
-      else{
-        stringInd +=1
+        patternInd = lpsArray[patternInd - 1];
+      }
+      else {
+        stringInd += 1
         patternInd = 0
       }
     }
@@ -42,3 +42,28 @@ function kpmSearch(string, pattern){
   return -1
 }
 
+
+// 746. Min Cost Climbing Stairs
+// On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+
+// Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+
+// Example 1:
+// Input: cost = [10, 15, 20]
+// Output: 15
+// Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+// Example 2:
+// Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+// Output: 6
+// Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+
+var minCostClimbingStairs = function (cost) {
+  if (cost.length === 2) return Math.min(cost[0], cost[1])
+  //array represents final and lowest cost to climb all stairs assuming that particular element in cost is the last one
+  const dp = [cost[0], Math.min(cost[1], cost[1] + cost[0])]
+  for (let i = 2; i < cost.length; i++) {
+    dp.push(cost[i] + Math.min(dp[i - 2], dp[i - 1]))
+  }
+  // then you return the lowest of the last two numbers in the array, since you can take either one or two steps
+  return Math.min(dp[dp.length - 1], dp[dp.length - 2])
+};
--- leetCodeQs.js
+++ leetCodeQs.js
@@ -2290,3 +2290,26 @@ var twoCitySchedCost = function (costs) {
   }
   return cityA + cityB
 };
+
+
+// Purhcasing Supplies
+// manufacturing company that provides customers with free containers to ship supplies. You have a cost of containers, an initial budget, a number of containers that need to be traded in for a free one.
+
+// example:
+// n(budget) = 4;
+// cost= 1;
+// m(free container) = 2; (trade 2, get 1 free)
+
+// first buy 4 containers with the entire budget bc one container costs 1;
+
+// 4 => 2 => 1
+//  output = 7 (total num of containers used)
+
+//  input:
+//  n = 6;
+//  cost = 2;
+//  m = 2;
+
+//  3 + 1 + 1 = 5
+//  output = 5;
+// */
