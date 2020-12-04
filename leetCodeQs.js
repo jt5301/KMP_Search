35bc7b1ef14701dcf5c8803f1b36b0c1759fec45
John Tieu <johnson.tieu@gmail.com>
2020-12-04 16:25:23 -0500

    sliding window maximum

--- leetCodeQs.js
+++ leetCodeQs.js
@@ -2695,9 +2695,9 @@ var findNumberOfLIS = function(nums) {
           if(nums[j] < nums[i]){
               if(memo[j]+1 > memo[i]){
                   memo[i] = memo[j]+1
-                  count[i] = count[j]
+                  count[i] = count[j]//this means there's a longer subseuence in the array. count is taken from previous biggest count.
               }
-              else if(memo[j]+1===memo[i]){
+              else if(memo[j]+1===memo[i]){//this means that there's a subsequence that is the same length as a previous subsequence. count is added on to current count
                   count[i]+=count[j]
               }
           }
@@ -2715,3 +2715,67 @@ var findNumberOfLIS = function(nums) {
   }
   return returnCount
 };
+
+// 239. Sliding Window Maximum
+// You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+// Return the max sliding window.
+
+// Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+// Output: [3,3,5,5,6,7]
+// Explanation:
+// Window position                Max
+// ---------------               -----
+// [1  3  -1] -3  5  3  6  7       3
+//  1 [3  -1  -3] 5  3  6  7       3
+//  1  3 [-1  -3  5] 3  6  7       5
+//  1  3  -1 [-3  5  3] 6  7       5
+//  1  3  -1  -3 [5  3  6] 7       6
+//  1  3  -1  -3  5 [3  6  7]      7
+
+
+var maxSlidingWindow = function(nums, k) {
+  let head = new Node(nums[0])
+  let reset = head
+  let maxArray = []
+  for(let i = 1;i<k;i++){
+      head.next = new Node(nums[i])
+      head = head.next
+  }
+  let largestVal = findLargest(reset)
+  maxArray.push(largestVal)
+  let counter = k
+  while(k < nums.length){
+      head.next = new Node(nums[k])
+      head = head.next
+      if(nums[k] >largestVal){
+          largestVal = nums[k]
+      }
+      else{
+          if(reset.val === largestVal){
+              let nextLargest = findLargest(reset.next)
+              largestVal = nextLargest
+          }
+      }
+      reset = reset.next
+      maxArray.push(largestVal)
+      k+=1
+  }
+  return maxArray
+};
+
+function findLargest(node,k){
+  let largest = -Infinity
+  while(node){
+      largest = Math.max(largest, node.val)
+      node = node.next
+  }
+  return largest
+}
+
+class Node{
+  constructor(val){
+      this.val = val
+      this.next = null
+  }
+}
