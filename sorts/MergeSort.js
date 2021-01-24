1251e558c337d2903589d8f4cf461f891196fdbc
John Tieu <johnson.tieu@gmail.com>
2021-01-24 17:57:01 -0500

    recreating merge sort

--- sorts/MergeSort.js
+++ sorts/MergeSort.js
@@ -41,5 +41,72 @@ function mergeSort(array){
 
 
 }
+//second implementation
+function mergeSort(array){
+  if(array.length===1)return array
+  let array1 = array.slice(0,array.length/2)
+  let array2 = array.slice(array.length/2)
+  return mergeSortHelper(mergeSort(array1),mergeSort(array2))
+}
+function mergeSortHelper(array1,array2){
+  let sorted = []
+  let arr1Point = 0
+  let arr2Point = 0
+  while(arr1Point < array1.length && arr2Point < array2.length){
+    if(array1[arr1Point] <= array2[arr2Point]){
+      sorted.push(array1[arr1Point])
+      arr1Point+=1
+    }
+    else{
+      sorted.push(array2[arr2Point])
+      arr2Point+=1
+    }
+  }
+  if(arr1Point < array1.length){
+    for(let i = arr1Point;i<array1.length;i++){
+      sorted.push(array1[arr1Point])
+    }
+  }
+  if(arr2Point < array2.length){
+    for(let i = arr2Point;i<array2.length;i++){
+      sorted.push(array2[arr2Point])
+    }
+  }
+  return sorted
+}
 
+function mergeSort(array){
+  if(array.length===1)return array
+  let array1 = array.slice(0,array.length/2)
+  let array2 = array.slice(array.length/2)
+  return mergeSortHelper(mergeSort(array1),mergeSort(array2))
+}
 
+function mergeSortHelper(array1,array2){
+  let arr1Count = 0
+  let arr2Count = 0
+  let sorted = []
+  while(arr1Count < array1.length && arr2Count < array2.length){
+    if(array1[arr1Count] < array2[arr2Count]){
+      sorted.push(array1[arr1Count])
+      arr1Count+=1
+    }
+    else{
+      sorted.push(array2[arr2Count])
+      arr2Count+=1
+    }
+  }
+  if(arr1Count < array1.length){
+    for(let i = arr1Count;i<array1.length;i++){
+      sorted.push(array1[i])
+    }
+  }
+  else{
+    for(let i = arr2Count;i < array2.length;i++){
+      sorted.push(array2[i])
+    }
+  }
+  return sorted
+}
+
+console.log(mergeSort([1,5,7,2,56,3,8,2,2,4,4]))
