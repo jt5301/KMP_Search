26708741973861cb7f02e6b40dc8de66842ba076
John Tieu <v-johntieu@microsoft.com>
2021-06-08 00:36:01 -0400

    min deletions v2

--- minDeletions.js
+++ minDeletions.js
@@ -21,3 +21,30 @@ var minDeletions = function(s) {
     }
     return count
 };
+
+var minDeletions = function (s) {
+    let minRemoves = 0
+    const letterHash = {}
+    for (let i = 0; i < s.length; i++) {
+        if (!letterHash[s[i]]) letterHash[s[i]] = 1
+        else letterHash[s[i]] += 1
+    }
+    let freqs = Object.values(letterHash)
+    freqs = freqs.sort((a, b) => {
+        return a - b
+    })
+    const freqsHash = {}
+    for (let i = 0; i < freqs.length; i++) {
+        if (!freqsHash[freqs[i]]) {
+            freqsHash[freqs[i]] = true
+        }
+        else {
+            while (freqsHash[freqs[i]] && freqs[i] != 0) {
+                freqs[i] -= 1
+                minRemoves += 1
+            }
+            freqsHash[freqs[i]] = true
+        }
+    }
+    return minRemoves
+};
